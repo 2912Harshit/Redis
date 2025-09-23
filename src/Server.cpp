d045c1454f7363c15ef0cdd0c1f340c2c77bf274
Harshit Aildasani <harshitaildasani@gmail.com>
2025-09-23 09:57:43 +0530

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -56,7 +56,7 @@ int main(int argc, char **argv) {
   int client_fd=accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
   std::cout << "Client connected\n";
-  string response="+PONG/r/n";
+  string response="+PONG\r\n";
   send(client_fd,response.c_str(),response.size(),0);
   close(client_fd);
   
