8c6889937c537e11ae7c02b6dcf4465a7faa0253
Harshit Aildasani <harshitaildasani@gmail.com>
2025-09-24 15:00:02 +0530

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -19,7 +19,8 @@ using namespace std;
 
 unordered_map<string,string>kv;
 unordered_map<string,chrono::steady_clock::time_point>expiry_map;
-mutex kv_mutex,expiry_map_mutex;
+vector<string>list_key;
+mutex kv_mutex,expiry_map_mutex,list_key_mutex;
 void start_expiry_cleaner(){
   thread([](){
     while(true){
@@ -39,6 +40,13 @@ void start_expiry_cleaner(){
   }).detach();
 }
 
+void handle_rpush_list_key(vector<string>&parsed_request){
+  lock_guard<mutex>lock1(list_key_mutex);
+  for(int i=2;i<parsed_request.size();i++){
+    list_key.push_back(parsed_request[i]);
+  }
+}
+
 void set_key_value(string key,string value,int delay_time){
     lock_guard<mutex>lock1(kv_mutex);
     lock_guard<mutex>lock2(expiry_map_mutex);
@@ -52,8 +60,10 @@ void set_key_value(string key,string value,int delay_time){
 void remove_key(string key){
     lock_guard<mutex>lock1(kv_mutex);
     lock_guard<mutex>lock2(expiry_map_mutex);
-    kv.erase(key);
-    expiry_map.erase(key);
+    if(expiry_map.count(key) && chrono::steady_clock::now()>=expiry_map[key]){
+      kv.erase(key);
+      expiry_map.erase(key);
+    }
 }
 
 // void set_timeout(int delayMs,string key){
@@ -63,28 +73,49 @@ void remove_key(string key){
 //   }).detach();
 // }
 
-string token_to_resp_bulk(string token){
-  string res="";
-  if(token.empty()){
-    res= "$-1\r\n";
-  }else if(token=="OK" || token=="PONG"){
-    res="+"+token+"\r\n";
-  }
-  else res= "$"+to_string(token.size())+"\r\n"+token+"\r\n";
-  return res;
-}
+// string token_to_resp_bulk(string token){
+//   string res="";
+//   if(token.empty()){
+//     res= "$-1\r\n";
+//   }else if(token=="OK" || token=="PONG"){
+//     res="+"+token+"\r\n";
+//   }
+//   else res= "$"+to_string(token.size())+"\r\n"+token+"\r\n";
+//   return res;
+// }
 
 void to_lowercase(string &str){
     transform(str.begin(),str.end(),str.begin(),
                   [](unsigned char c){return tolower(c);});
     
 }
-
-
-void send_string_wrap(int client_fd,string msg){
-  string resp_bulk=token_to_resp_bulk(msg);
-  send(client_fd,resp_bulk.c_str(),resp_bulk.size(),0);
+void send_simple_string(int client_fd,string msg){
+  string resp_simple;
+  resp_simple="+"+msg+"\r\n";
+  send(client_fd,resp_simple.c_str(),resp_simple.size(),0);
 }
+void send_bulk_string(int client_fd,string msg){
+  string resp_simple;
+  resp_simple="$"+to_string(msg.size())+"\r\n"+msg+"\r\n";
+  send(client_fd,resp_simple.c_str(),resp_simple.size(),0);
+}
+void send_integer(int client_fd,int msg){
+  string resp_simple;
+  resp_simple=":"+to_string(msg)+"\r\n";
+  send(client_fd,resp_simple.c_str(),resp_simple.size(),0);
+}
+
+void send_null_string(int client_fd){
+  string resp_simple="$-1\r\n";
+  send(client_fd,resp_simple.c_str(),resp_simple.size(),0);
+}
+
+
+
+// void send_string_wrap(int client_fd,string msg){
+//   string resp_bulk=token_to_resp_bulk(msg);
+//   send(client_fd,resp_bulk.c_str(),resp_bulk.size(),0);
+// }
 void handleResponse(int client_fd){
   char buffer[1024];
   while(true){
@@ -108,9 +139,9 @@ void handleResponse(int client_fd){
     to_lowercase(command);
     string response="";
     if(command=="ping"){
-      send_string_wrap(client_fd,"PONG");
+      send_simple_string(client_fd,"PONG");
     }else if(command=="echo"){
-        send_string_wrap(client_fd,parsed_request[1]);
+        send_bulk_string(client_fd,parsed_request[1]);
     }else if(command=="set"){
       string key=parsed_request[1];
       string value=parsed_request[2];
@@ -124,17 +155,32 @@ void handleResponse(int client_fd){
         }
       }
       set_key_value(key,value,delay_time);
-      send_string_wrap(client_fd,"OK");
-    }else if(command=="get"){
-      string key=parsed_request[1];
-      if(kv.count(key)){
-        if(!expiry_map.count(key) || chrono::steady_clock::now()<expiry_map[key])send_string_wrap(client_fd,kv[key]);
-        else{
-          remove_key(key);
-          send_string_wrap(client_fd,"");
+      send_simple_string(client_fd,"OK");
+    } else if(command=="get") {
+        string key = parsed_request[1];
+        bool send_empty = false;
+        string value;
+        {
+            lock_guard<mutex> lock1(kv_mutex);
+            lock_guard<mutex> lock2(expiry_map_mutex);
+            if(kv.count(key)){
+                if(!expiry_map.count(key) || chrono::steady_clock::now() < expiry_map[key])
+                    value = kv[key];
+                else
+                    send_empty = true; // mark to remove later
+            } else
+                send_empty = true;
         }
+        if(send_empty) remove_key(key); // lock again inside remove_key is fine
+        if(!value.empty()) send_bulk_string(client_fd, value);
+        else send_null_string(client_fd);
+    }else if(command=="rpush"){
+      string internal_command=parsed_request[1];
+      to_lowercase(internal_command);
+      if(internal_command=="list_key"){
+        handle_rpush_list_key(parsed_request);
+        send_integer(client_fd,list_key.size());
       }
-      else send_string_wrap(client_fd,"");
     }
   }
 }
