2baf614ea3f5a26993318704e68c5055a6739cdf
Harshit Aildasani <harshitaildasani@gmail.com>
2025-09-24 23:03:33 +0530

    [any message]

--- src/handlers.h
+++ src/handlers.h
@@ -7,7 +7,7 @@ int handle_rpush(std::vector<std::string> &parsed_request, std::string &key);
 int handle_lpush(std::vector<std::string> &parsed_request, std::string &key);
 std::string handle_lpop(std::string &key);
 void handle_multiple_lpop(int client_fd, std::string &key, int no_of_removals);
-void handle_blpop(int client_fd, std::string &key, float time = 0);
+void handle_blpop(int client_fd, std::string &key, float time);
 
 void set_key_value(std::string key, std::string value, int delay_time);
 void remove_key(std::string key);
