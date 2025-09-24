e2d813e9e52fca355d722004a1eaa8c68fb6038c
Harshit Aildasani <harshitaildasani@gmail.com>
2025-09-24 21:18:40 +0530

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -52,6 +52,41 @@ string create_resp_array(int client_fd, deque<string> &list, int start = 0, int 
   cout << endl;
   return resp_array;
 }
+void send_simple_string(int client_fd, string msg)
+{
+  string resp_simple;
+  resp_simple = create_simple_string(msg);
+  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
+}
+void send_bulk_string(int client_fd, string msg)
+{
+  string resp_simple;
+  resp_simple = create_bulk_string(msg);
+  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
+}
+void send_integer(int client_fd, int msg)
+{
+  string resp_simple;
+  resp_simple = create_integer(msg);
+  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
+}
+
+void send_null_string(int client_fd)
+{
+  string resp_simple = "$-1\r\n";
+  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
+}
+void send_null_array(int client_fd)
+{
+  string resp_null_array = "*0\r\n";
+  send(client_fd, resp_null_array.c_str(), resp_null_array.size(), 0);
+}
+void send_array(int client_fd, deque<string> &list, int start = 0, int end = INT_MAX)
+{
+  lock_guard<mutex> lock1(lists_mutex);
+  string resp_array = create_resp_array(client_fd, list, start, end);
+  send(client_fd, resp_array.c_str(), resp_array.size(), 0);
+}
 void start_expiry_cleaner()
 {
   thread([]()
@@ -205,41 +240,6 @@ void to_lowercase(string &str)
   transform(str.begin(), str.end(), str.begin(),
             [](unsigned char c)
             { return tolower(c); });
-}
-void send_simple_string(int client_fd, string msg)
-{
-  string resp_simple;
-  resp_simple = create_simple_string(msg);
-  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
-}
-void send_bulk_string(int client_fd, string msg)
-{
-  string resp_simple;
-  resp_simple = create_bulk_string(msg);
-  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
-}
-void send_integer(int client_fd, int msg)
-{
-  string resp_simple;
-  resp_simple = create_integer(msg);
-  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
-}
-
-void send_null_string(int client_fd)
-{
-  string resp_simple = "$-1\r\n";
-  send(client_fd, resp_simple.c_str(), resp_simple.size(), 0);
-}
-void send_null_array(int client_fd)
-{
-  string resp_null_array = "*0\r\n";
-  send(client_fd, resp_null_array.c_str(), resp_null_array.size(), 0);
-}
-void send_array(int client_fd, deque<string> &list, int start = 0, int end = INT_MAX)
-{
-  lock_guard<mutex> lock1(lists_mutex);
-  string resp_array = create_resp_array(client_fd, list, start, end);
-  send(client_fd, resp_array.c_str(), resp_array.size(), 0);
 }
 
 void handleResponse(int client_fd)
